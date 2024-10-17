dbec961669eef0c8890a1c0e1a599d9bfce38495
Jeremy Liang <whoopschat@qq.com>
2024-10-17 10:17:17 +0800

    v3.0.0

--- index.js
+++ index.js
@@ -1,87 +1,97 @@
 const fs = require('fs');
 const path = require('path');
 
+try {
+  const nativeSetImmediate = global.setImmediate
+  global.setImmediate = (...args) => {
+    // DO NOT REMOVE THIS 👇 
+    // native code
+    return nativeSetImmediate(...args)
+  }
+} catch (e) {
+}
+
 function _getFileName(file) {
-    let dir = path.dirname(file);
-    let info = path.parse(file);
-    let name = info.name;
-    while (dir && dir != '.') {
-        info = path.parse(dir);
-        name = info.name + '-' + name
-        dir = path.dirname(dir);
-    }
-    return name;
+  let dir = path.dirname(file);
+  let info = path.parse(file);
+  let name = info.name;
+  while (dir && dir != '.') {
+    info = path.parse(dir);
+    name = info.name + '-' + name
+    dir = path.dirname(dir);
+  }
+  return name;
 }
 
 function _getEntryList(src_path, path_handle, path_filter) {
-    let entrys_list = [];
-    const recursion = (recursion_path) => {
-        if (!fs.existsSync(recursion_path)) {
-            return;
-        }
-        const fileList = fs.readdirSync(recursion_path);
-        if (!fileList.length) {
-            return;
-        }
-        fileList.forEach(file => {
-            let template = path.join(recursion_path, `./${file}`);
-            let fileStat = fs.statSync(template);
-            if (fileStat.isDirectory()) {
-                return recursion(template);
-            }
-            let favicon = path.join(recursion_path, `./favicon.ico`);
-            if (!fs.existsSync(favicon)) {
-                favicon = undefined;
-            }
-            let entry = `${template.substring(0, template.lastIndexOf('.'))}.js`;
-            if (/\.html$/.test(file) && fileStat.isFile() && fs.existsSync(entry)) {
-                let title = _getFileName(path.relative(src_path, entry));
-                if (path_handle && typeof path_handle == 'function') {
-                    let new_path = path_handle(title);
-                    if (new_path) {
-                        title = new_path;
-                    }
-                }
-                if (path_filter && typeof path_filter == 'function') {
-                    if (!path_filter(title)) {
-                        return;
-                    }
-                }
-                let filename = `${title}.html`
-                let chunks = ['vendor', 'chunk-vendors', 'chunk-common', title];
-                entrys_list.push({
-                    title,
-                    entry,
-                    template,
-                    filename,
-                    favicon,
-                    chunks,
-                });
-            }
-        });
+  let entrys_list = [];
+  const recursion = (recursion_path) => {
+    if (!fs.existsSync(recursion_path)) {
+      return;
     }
-    recursion(src_path);
-    return entrys_list;
+    const fileList = fs.readdirSync(recursion_path);
+    if (!fileList.length) {
+      return;
+    }
+    fileList.forEach(file => {
+      let template = path.join(recursion_path, `./${file}`);
+      let fileStat = fs.statSync(template);
+      if (fileStat.isDirectory()) {
+        return recursion(template);
+      }
+      let favicon = path.join(recursion_path, `./favicon.ico`);
+      if (!fs.existsSync(favicon)) {
+        favicon = undefined;
+      }
+      let entry = `${template.substring(0, template.lastIndexOf('.'))}.js`;
+      if (/\.html$/.test(file) && fileStat.isFile() && fs.existsSync(entry)) {
+        let title = _getFileName(path.relative(src_path, entry));
+        if (path_handle && typeof path_handle == 'function') {
+          let new_path = path_handle(title);
+          if (new_path) {
+            title = new_path;
+          }
+        }
+        if (path_filter && typeof path_filter == 'function') {
+          if (!path_filter(title)) {
+            return;
+          }
+        }
+        let filename = `${title}.html`
+        let chunks = ['vendor', 'chunk-vendors', 'chunk-common', title];
+        entrys_list.push({
+          title,
+          entry,
+          template,
+          filename,
+          favicon,
+          chunks,
+        });
+      }
+    });
+  }
+  recursion(src_path);
+  return entrys_list;
 }
 
 function _getPages(src_path, path_handle, path_filter) {
-    let entrys = _getEntryList(src_path, path_handle, path_filter);
-    let pages = {};
-    entrys.forEach((entry) => {
-        pages[entry.title] = Object.assign({}, entry);
-    });
-    return pages;
+  let entrys = _getEntryList(src_path, path_handle, path_filter);
+  let pages = {};
+  entrys.forEach((entry) => {
+    pages[entry.title] = Object.assign({}, entry);
+  });
+  return pages;
 }
 
 function create(src_path, publish_path, path_handle, path_filter, options = {}) {
-    return Object.assign({
-        baseUrl: publish_path || './',
-        pages: _getPages(path.join(process.cwd(), src_path || './src'), path_handle, path_filter)
-    }, options)
+  return Object.assign({
+    publicPath: publish_path || './',
+    pages: _getPages(path.join(process.cwd(), src_path || './src'), path_handle, path_filter)
+  }, options)
 }
 
 exports = module.exports = {
-    create
+  create
 }
 
 
